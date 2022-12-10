e72e78334d94f49e4bf186610ae27ee7418857ed
neetu_chaurasiya <neetu@gma8il.com>
2022-12-10 22:21:34 +0530

    initial modification

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+<h1>{{ title }}</h1>
+<p>Welcome.....!</p>
+<!-- <br>
+<input type="text" ref="name">
+<button @click="handleRef">chnage ref</button> -->
+  <div v-if="showModal">
+ <Modal :header="header" :text="text"  theme="Sale" @click="toggleButton"/>
+ 
+  </div>
+  <button @click.right="toggleButton">open Modal</button>
+  </template>
+
+<script >
+
+import Modal from './components/Modal.vue'
+
+export default  {
+  name : 'App' ,
+  components : { Modal },
+  data(){
+    return {
+      title :'My first vue app:',
+      header :'sign up for the Giveaway',
+      text: 'Grab your ninjs sawag for half prove!',
+     showModal : false
+    }
+  },
+  methods : {
+  handleRef(){
+   console.log(this.$refs.name)
+   this.$refs.name.classList.add('active')
+   this.$refs.name.focus()
+  },
+  toggleButton(){
+    this.showModal = !this.showModal
+  }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="backdrop" @click.prevent="closeModel">
+        <div class="modal" :class="{ Sale: theme ==='Sale'}">
+            <h1>{{ header}}</h1>
+            <p>{{ text}}</p>
+        
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props : ['header','text','theme'],
+    methods: {
+        closeModel(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+<style scoped>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius:  10px;
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    background:  rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+
+}
+h1{
+    color: aqua;
+    border: none;
+    padding: 0;
+}
+.modal p{
+font-style: normal;
+}
+.modal.Sale {
+    background: crimson;
+ color: white;
+}
+.modal.Sale h1{
+    color: white;
+}
+</style>
